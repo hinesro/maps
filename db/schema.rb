5ccc41005b6f73f6f170d49155c08f0ac03ae5e4
Robert Hines <hinesro@onid.oregonstate.edu>
2014-12-07 21:31:59 -0800

    Change listing description from string to text to allow more characters to be entered

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207225654) do
+ActiveRecord::Schema.define(version: 20141208053103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20141207225654) do
 
   create_table "listings", force: true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.float    "price"
     t.string   "status"
     t.datetime "created_at"
